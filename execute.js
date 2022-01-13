9547a45ffa4f79a0f65fd4c4a61d880bf6c9736a
Cherry Wang <cherryw2001@hotmail.com>
2022-01-13 20:16:36 +1100

    changed to popup on click

--- execute.js
+++ execute.js
@@ -1,4 +1,10 @@
 // this code will be executed when the extension's button is clicked
 (function() {
   console.log('execute.js executed');
+  const background = document.getElementById("only-recipe-background");
+  const wrapper = document.getElementById("only-recipe-wrapper");
+
+  background.style.display = "block";
+  wrapper.style.display = "block";
 })();
+
